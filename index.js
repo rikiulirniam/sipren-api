c8f5173e7839f0f77006a277cad6f4b02e4e1f18
rikiwebsite <rikiwebdev@gmail.com>
2024-03-10 06:14:07 +0700

    feat: seperate the routes to different file

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ const port = process.env.PORT || 8080;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
+app.get("/", require("./routes/main"));
 
 app.listen(port, () => {
-  console.log("Server running");
+  console.log(
+    "[server] server berhasil dijalankan di http://127.0.0.1:" + port
+  );
 });
