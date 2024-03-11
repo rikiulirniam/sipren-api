880c482ef7ac1b8b3c795ef082044c13a17fd0a2
PraditaAdiantama <aditadiantama@gmail.com>
2024-03-11 21:01:45 +0700

    feat: add env

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
+const dotenv = require('dotenv');
+dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
