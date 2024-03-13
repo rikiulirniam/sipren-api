b8d0e809bc0e168de98dfc96b3d260096c7cd9ed
PraditaAdiantama <mpaproject307@gmail.com>
2024-03-13 07:41:04 +0700

    feat: add kelas model

---
+++ models/kelas.js
@@ -0,0 +1,20 @@
+const db = require('../utils/db');
+
+class Kelas {
+    static all(id_jurusan, id_tingkat) {
+        return new Promise((resolve, reject) => {
+            let q = "SELECT * FROM kelas";
+
+            if (id_jurusan) q += (q[q.length - 1] == 's' ? ' WHERE' : ' AND') + ` id_jurusan = ${id_jurusan}`;
+            if (id_tingkat) q += (q[q.length - 1] == 's' ? ' WHERE' : ' AND') + ` id_tingkat = ${id_tingkat}`
+
+            db.query(q, (err, data) => {
+                if (err) reject(err);
+
+                resolve(data);
+            })
+        })
+    }
+}
+
+module.exports = Kelas;
