cf223b5c38356877cdd5f08eac46e0e0f9fa749d
rikiulirniam <rikiulir@gmail.com>
2024-11-07 23:21:55 +0700

    hh

--- models/detailPresensi.js
+++ models/detailPresensi.js
@@ -4,7 +4,7 @@ class DetailPresensi {
   static all() {
     return new Promise((resolve, reject) => {
       let q =
-        "SELECT det_presensi.id_det, materi.nama_materi, materi.deskripsi, user.nama, kelas.tingkat, kelas.no_kelas, jurusan.akronim, presensi.deskripsi, siswa.nama, det_presensi.keterangan FROM det_presensi INNER JOIN presensi ON det_presensi.id_presensi = presensi.id_presensi INNER JOIN materi ON presensi.id_materi = materi.id_materi INNER JOIN user ON presensi.id_user = user.id_user INNER JOIN kelas ON presensi.id_kelas = kelas.id_kelas INNER JOIN siswa ON det_presensi.id_siswa = siswa.nis INNER JOIN jurusan ON kelas.id_jurusan = jurusan.id_jurusan";
+        "SELECT det_presensi.id_det, materi.nama_materi, materi.deskripsi, user.nama, kelas.tingkat, kelas.no_kelas, jurusan.akronim,  siswa.nama, det_presensi.keterangan FROM det_presensi INNER JOIN presensi ON det_presensi.id_presensi = presensi.id_presensi INNER JOIN materi ON presensi.id_materi = materi.id_materi INNER JOIN user ON presensi.id_user = user.id_user INNER JOIN kelas ON presensi.id_kelas = kelas.id_kelas INNER JOIN siswa ON det_presensi.id_siswa = siswa.nis INNER JOIN jurusan ON kelas.id_jurusan = jurusan.id_jurusan";
 
       db.query(q, (err, res) => {
         if (err) reject(err);
--- models/presensi.js
+++ models/presensi.js
@@ -1,77 +1,92 @@
-const db = require('../utils/db');
+const db = require("../utils/db");
 
-class Presensi{
-    static all(){
-        return new Promise((resolve, reject) => {
-            let q = "SELECT materi.nama_materi, user.nama, kelas.no_kelas, presensi.jam_started, presensi.jam_ended , presensi.deskripsi, presensi.hari_tanggal, presensi.create_date, presensi.update_date FROM presensi INNER JOIN materi ON presensi.id_materi = materi.id_materi INNER JOIN user ON presensi.id_user = user.id_user INNER JOIN kelas ON presensi.id_kelas = kelas.id_kelas"
+class Presensi {
+  static all() {
+    return new Promise((resolve, reject) => {
+      let q =
+        "SELECT materi.nama_materi, user.nama, kelas.no_kelas, presensi.jam_started, presensi.jam_ended , presensi.hari_tanggal, presensi.create_date, presensi.update_date FROM presensi INNER JOIN materi ON presensi.id_materi = materi.id_materi INNER JOIN user ON presensi.id_user = user.id_user INNER JOIN kelas ON presensi.id_kelas = kelas.id_kelas";
 
-            db.query(q, (err, res) => {
-                if(err) reject(err);
-                else resolve(res);
-            })
-        })
-    }
+      db.query(q, (err, res) => {
+        if (err) reject(err);
+        else resolve(res);
+      });
+    });
+  }
 
-    static create(values){
-        return new Promise((resolve, reject) => {
-            let q = "INSERT INTO presensi(`id_materi`, `id_user`, `id_kelas`, `jam_started` , `jam_ended` , `hari_tanggal`) VALUES(?)"
+  static create(values) {
+    return new Promise((resolve, reject) => {
+      let q =
+        "INSERT INTO presensi(`id_materi`, `id_user`, `id_kelas`, `jam_started` , `jam_ended` , `hari_tanggal`) VALUES(?)";
 
-            db.query(q, [values], (err, res) => {
-                if(err) reject(err);
+      db.query(q, [values], (err, res) => {
+        if (err) reject(err);
 
-                if(res && res.insertId){
-                    resolve(res.insertId)
-                }else{
-                    reject(new Error("tidak dapat mengembalikan id"))
-                }
-                
-            })
-        })
-    } 
+        if (res && res.insertId) {
+          resolve(res.insertId);
+        } else {
+          reject(new Error("tidak dapat mengembalikan id"));
+        }
+      });
+    });
+  }
 
-    static update(id_materi, id_user, id_kelas, deskripsi, hari_tanggal, id_presensi){
-        return new Promise((resolve, reject) => {
-            let q = "UPDATE presensi SET id_materi = ?, id_user = ?, id_kelas = ?, deskripsi = ?, hari_tanggal = ? WHERE id_presensi = ?"
-            
-            db.query(q, [id_materi, id_user, id_kelas, deskripsi, hari_tanggal, id_presensi], (err, res) => {
-                if(err) reject(err);
-                else resolve(res);
-            })
-        })
-    }
+  static update(
+    id_materi,
+    id_user,
+    id_kelas,
+    deskripsi,
+    hari_tanggal,
+    id_presensi
+  ) {
+    return new Promise((resolve, reject) => {
+      let q =
+        "UPDATE presensi SET id_materi = ?, id_user = ?, id_kelas = ?, deskripsi = ?, hari_tanggal = ? WHERE id_presensi = ?";
 
-    static delete(id_presensi){
-        return new Promise((resolve, reject) => {
-            let q = "DELETE FROM presensi WHERE id_presensi = ?";
+      db.query(
+        q,
+        [id_materi, id_user, id_kelas, deskripsi, hari_tanggal, id_presensi],
+        (err, res) => {
+          if (err) reject(err);
+          else resolve(res);
+        }
+      );
+    });
+  }
 
-            db.query(q, [id_presensi], (err, res) => {
-                if(err) reject(err);
-                else resolve(res);
-            })
-        })
-    }
+  static delete(id_presensi) {
+    return new Promise((resolve, reject) => {
+      let q = "DELETE FROM presensi WHERE id_presensi = ?";
 
-    static findByKelas(id_kelas){
-        return new Promise((resolve, reject) => {
-            let q = "SELECT materi.nama_materi, user.nama, kelas.id_kelas, presensi.deskripsi, presensi.hari_tanggal, presensi.create_date, presensi.update_date FROM presensi INNER JOIN materi ON presensi.id_materi = materi.id_materi INNER JOIN user ON presensi.id_user = user.id_user INNER JOIN kelas ON presensi.id_kelas = kelas.id_kelas WHERE kelas.id_kelas = ?";
-            
-            db.query(q, [id_kelas], (err, res) =>{
-                if(err) reject(err);
-                else resolve(res);
-            })
-        })
-    }
+      db.query(q, [id_presensi], (err, res) => {
+        if (err) reject(err);
+        else resolve(res);
+      });
+    });
+  }
 
-    static findByPresensi(id_presensi){
-        return new Promise((resolve, reject) => {
-            let q = "SELECT materi.nama_materi, user.nama, kelas.no_kelas, presensi.deskripsi, presensi.hari_tanggal, presensi.create_date, presensi.update_date FROM presensi INNER JOIN materi ON presensi.id_materi = materi.id_materi INNER JOIN user ON presensi.id_user = user.id_user INNER JOIN kelas ON presensi.id_kelas = kelas.id_kelas WHERE presensi.id_presensi = ?";
-            
-            db.query(q, [id_presensi], (err, res) =>{
-                if(err) reject(err);
-                else resolve(res);
-            })
-        })
-    }
+  static findByKelas(id_kelas) {
+    return new Promise((resolve, reject) => {
+      let q =
+        "SELECT materi.nama_materi, user.nama, kelas.id_kelas, presensi.hari_tanggal, presensi.create_date, presensi.update_date FROM presensi INNER JOIN materi ON presensi.id_materi = materi.id_materi INNER JOIN user ON presensi.id_user = user.id_user INNER JOIN kelas ON presensi.id_kelas = kelas.id_kelas WHERE kelas.id_kelas = ?";
+
+      db.query(q, [id_kelas], (err, res) => {
+        if (err) reject(err);
+        else resolve(res);
+      });
+    });
+  }
+
+  static findByPresensi(id_presensi) {
+    return new Promise((resolve, reject) => {
+      let q =
+        "SELECT materi.nama_materi, user.nama, kelas.no_kelas, presensi.hari_tanggal, presensi.create_date, presensi.update_date FROM presensi INNER JOIN materi ON presensi.id_materi = materi.id_materi INNER JOIN user ON presensi.id_user = user.id_user INNER JOIN kelas ON presensi.id_kelas = kelas.id_kelas WHERE presensi.id_presensi = ?";
+
+      db.query(q, [id_presensi], (err, res) => {
+        if (err) reject(err);
+        else resolve(res);
+      });
+    });
+  }
 }
 
 module.exports = Presensi;
