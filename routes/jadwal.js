c883e94e868f81f8faef61f1a7ab012eb113fbc8
rikiulirniam <rikiulir@gmail.com>
2025-07-06 20:46:36 +0700

    jadwal:UD

--- routes/jadwal.js
+++ routes/jadwal.js
@@ -3,6 +3,10 @@ const controller = require("../controllers/jadwal");
 const Verify = require("../middleware/verifyUser");
 
 router.post("/", Verify.verifyLevel, controller.create);
-router.get("/", controller.index);
+router.get("/",Verify.verifyLevel, controller.all)
+router.get("/mine", controller.index);
+router.get("/:id_jadwal",Verify.verifyLevel, controller.detail)
+router.put("/:id_jadwal", Verify.verifyLevel, controller.update);
+router.delete("/:id_jadwal", Verify.verifyLevel, controller.delete)
 
 module.exports = router
